f8d46a8d3bf4e2cab26866bbf15024af7bcbdd41
hellwail <superwow0887@gmail.com>
2023-03-21 14:04:42 +0300

    added webpack and sass

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './style.scss';
+
+document.write("Hello, WebPack1111111!!!!");
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: "main.js"
+    },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+        
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                
+            }
+        ]
+    }
+};
+
